742a48574e8976b05848eccd82e65846e1f7e342
Jon Chen <bsd@voltaire.sh>
2024-02-18 03:12:17 -0800

    draw a circle in prep for velocity

--- lib/Screen/Screen.h
+++ lib/Screen/Screen.h
@@ -4,4 +4,12 @@
 #define SCREEN_WIDTH 320
 #define SCREEN_HEIGHT 240
 
+#define CS_PIN 20
+#define RST_PIN 19
+#define DC_PIN 18
+#define SCK_PIN 2
+#define TX_PIN 3
+
+#define NEON_GREEN 0x17EA
+
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,15 @@ extern "C"
 #include <gfx.h>
 }
 
-unsigned int cs = 20;
-unsigned int rst = 19;
-unsigned int dc = 18;
-unsigned int sck = 2;
-unsigned int tx = 3;
-unsigned int counter = 0;
+// pin definitions
 
 void setup()
 {
   // put your setup code here, to run once:
-  LCD_setPins(dc, cs, rst, sck, tx);
+  LCD_setPins(DC_PIN, CS_PIN, RST_PIN, SCK_PIN, TX_PIN);
   LCD_initDisplay();
   LCD_setRotation(1);
   GFX_createFramebuf();
-}
-
-void moveCircle(unsigned int counter)
-{
-  unsigned int x = counter * 5;
-
-  if (x > SCREEN_WIDTH)
-  {
-    counter = 0;
-    moveCircle(counter);
-    return;
-  }
-
-  GFX_fillCircle(x, 160, 50, 0x00FF00);
-  counter++;
-  return;
 }
 
 void loop()
@@ -44,8 +23,8 @@ void loop()
   // put your main code here, to run repeatedly:
   GFX_clearScreen();
   GFX_setCursor(0, 0);
-  GFX_printf("Hello GFX!\n%d", counter);
-  moveCircle(counter);
+  GFX_printf("Hello GFX!\n");
+  GFX_drawCircle(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2, 80, NEON_GREEN); // center circle
   GFX_flush();
   delay(500);
 }
